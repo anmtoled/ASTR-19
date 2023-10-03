b541e24262f0e9d39e5eb5f55a4ba7708acd6f15
anmtoled <95516630+anmtoled@users.noreply.github.com>
2023-10-03 11:31:37 -0700

    Session 1 prompt for Coding Journal 1

---
+++ session-one-prompt.py
@@ -0,0 +1,15 @@
+#Anna Toledo
+#Session One Prompt One
+
+#Write Python program to print out:
+#Full name, preferred pronouns, and 2 sentences about favorite movie and favorite food
+
+def PrintAboutMe():
+	print("My name is Anna Toledo. My preferred pronouns are they/them.")
+	print("My favorite movie is \"The Thing.\" \nMy favorite food is sushi!")
+
+def main():
+	PrintAboutMe()
+
+if __name__ == '__main__':
+	main()
